793fc508baaaf3848b73e49a1f9338ce37a12f4d
Kaviraghul <vettrithamil@gmail.com>
2023-05-18 09:39:56 +0530

    addition of app colors

---
+++ lib/presentation/resources/colors_manager.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColorsManager {
+  static const Color primaryColor = Color(0xFFF9F9F9);
+  static const Color blackColor = Color(0xFF222222);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color greenColor = Color(0xFF2AA952);
+  static const Color grayColor = Color(0xFF9B9B9B);
+  static const Color buttonRedColor = Color(0xFFDB3022);
+  static const Color errorRedColor = Color(0xFFF01F0E);
+  static const Color starColor = Color(0xFFFFBA49);
+}
