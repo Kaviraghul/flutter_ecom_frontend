3bf207ec954cdf9f6755b76b4599d8e8936d22a5
Kaviraghul <vettrithamil@gmail.com>
2023-05-18 10:03:08 +0530

    fonts manager addition

--- lib/presentation/resources/colors_manager.dart
+++ lib/presentation/resources/colors_manager.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class ColorsManager {
   static const Color primaryColor = Color(0xFFF9F9F9);
+  static const Color secondaryColor = Color(0xFF000000);
   static const Color blackColor = Color(0xFF222222);
   static const Color whiteColor = Color(0xFFFFFFFF);
   static const Color greenColor = Color(0xFF2AA952);
