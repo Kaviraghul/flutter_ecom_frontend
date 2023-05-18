28f2f73f610fb5c246ccd9f1ad46a34e0c62319c
Kaviraghul <vettrithamil@gmail.com>
2023-05-18 10:12:08 +0530

    strings manager addition

---
+++ lib/presentation/resources/strings_manage.dart
@@ -0,0 +1,19 @@
+class AppStrings {
+  // Authentication
+  static const String signUp = 'Sign up';
+  static const String register = 'Register';
+  static const String forgotPassword = 'Forgot your password';
+  static const String existingAccount = 'Already have an account?';
+  static const String newAccount = 'Don\'t have an account?';
+  static const String mailVerification =
+      'Please, enter your email address. You will receive a link to create a new password via email.';
+  static const String mailInputError =
+      'Not a valid email address. Should be your@email.com';
+  static const String socialSignup = 'Or sign up with social account';
+  static const String socialLogin = 'Or sign up with social account';
+
+  // User
+  static const String name = 'Name';
+  static const String email = 'Email';
+  static const String password = 'Password';
+}
