62a69d0179bcb0c18b41adf237d41c515f09b2e6
Kaviraghul <vettrithamil@gmail.com>
2023-05-18 11:28:57 +0530

    added values manager

--- lib/presentation/resources/text_manager.dart
+++ lib/presentation/resources/text_manager.dart
@@ -13,10 +13,7 @@ class FontWeightManager {
 }
 
 class FontSize {
-  static const double s12 = 12.0;
+  static const double s11 = 11.0;
   static const double s14 = 14.0;
-  static const double s16 = 16.0;
-  static const double s17 = 17.0;
-  static const double s18 = 18.0;
-  static const double s20 = 20.0;
+  static const double s34 = 34.0;
 }
